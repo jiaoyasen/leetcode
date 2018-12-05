930a9b8722e24a3f5a6ed73d8150eb9d6ddb6800
jiaoyasen <jiaoyasen@tju.edu.cn>
2018-12-05 15:02:34 +0800

    Update test1 with 哲の王.py

--- test1 with 哲の王.py
+++ test1 with 哲の王.py
@@ -1,9 +1,33 @@
 import operator
+import datetime
+import collections
+import time
 data = [1,3,7,2,11,35,9,7,3,1,1,3,4,5,23]
 result = {}
+
+time1 = time.clock()
 for i in data:
-    tmp = result.get(i,0) + 1
-    result[i] = tmp
+    result[i] = result.get(i,0) + 1
+time2 = time.clock()
+print("The running time of 1 is ",(time2-time1))
 print(result)
-result_sorted = sorted(result.items(), key = operator.itemgetter(0))
-print(result_sorted)
+
+result = {}
+time1 = time.clock()
+a = set(data)
+for i in a:
+    result[i] = data.count(i)
+time2 = time.clock()
+print("The running time of 2 is ",(time2-time1))
+print(result)
+
+result = {}
+time1 = time.clock()
+result = collections.Counter(data)
+time2 = time.clock()
+print("The running time of 2 is ",(time2-time1))
+print(result)
+
+
+# result_sorted = sorted(result.items(), key = operator.itemgetter(0))
+# print(result_sorted)
