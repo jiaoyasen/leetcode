8a1efc669e9b60b7349c10bbcfe7464604d5636b
jiaoyasen <jiaoyasen@tju.edu.cn>
2018-12-08 07:33:45 +0800

    Create 832. Flipping an Image.py

---
+++ 832. Flipping an Image.py
@@ -0,0 +1,11 @@
+class Solution:
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        for i in A:
+            i.reverse()
+            for j in range(len(i)):
+                i[j] ^= 1
+        return(A)
